5e970a0a573b900ef3beeb208d647e185325fbbc
hjnsjd <hjnsjd@naver.com>
2021-05-30 15:41:22 +0900

    210530_Sample01
    UI,view

--- app/src/main/java/com/hjnsjd/sample/MainActivity.kt
+++ app/src/main/java/com/hjnsjd/sample/MainActivity.kt
@@ -4,33 +4,30 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.hjnsjd.sample.databinding.ActivityMainBinding
+
+/**
+ * DataBinding 사용하는 법 www.ienlab.net/entry/안드로이드-Unresolved-reference-오류-데이터바인딩-사용하기
+ */
 
 val TAG = "sampleTAG"
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.activity = this
 
-        Log.d(TAG, "hello world")
+//        val textView: TextView = findViewById(R.id.textView)
+//        textView.text = "김형진"
 
-        val animal = Animal(5, 0, "A")
-        animal.breathe()
-
-        animal.move(50)
-        val human = Human(5, 1, "Ba", "050801", "USA")
-        human.talk()
-        human.eat("사과")
-        human.think()
-
-        val student = Student(10, 0, "hjn", "050801", "Korea", 10505, 90f)
-
-
-        if (student.test("math")) {
-            Log.d(TAG, "시험을 잘 쳤다.")
-        } else {
-            Log.d(TAG, "시험을 못 쳤다.")
-        }
+        var a="과로사"
+        binding.textView.text = a
 
 
     }
